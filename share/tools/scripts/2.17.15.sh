0104757170a248410a6f52e4486c93c9f716565e
Leonid Kogan <leon@leonsio.com>
2016-04-29 12:40:26 +0200

    Bugfix Installation Addons

--- share/tools/scripts/2.17.15.sh
+++ share/tools/scripts/2.17.15.sh
@@ -29,3 +29,39 @@ echo '#!/bin/sh' > ${ROOT}/bin/update_firmware_run
 sed -i "s/modprobe/echo/g" ${ROOT}/etc/init.d/S00eQ3SystemStart
 touch ${ROOT}/usr/local/etc/config/no-coprocessor-update
 mv ${ROOT}/etc/init.d/S40UsbNetwork  ${ROOT}/etc/init.d/_S40UsbNetwork
+cat >> "${ROOT}/bin/update_firmware_run"  <<EOF
+log() {
+  logger -t homematic -p user.notice $1
+}
+
+echo "YAHM: Entering addon install mode"
+log "YAHM: Entering addon install mode"
+if [ ! -f /var/new_firmware.tar.gz ]; then
+  log "YAHM: addon image archive does not exist. Nothing to do"
+  echo "YAHM: addon image archive does not exist. Nothing to do"
+  exit
+fi
+
+echo "YAHM: extract addon archive"
+log "YAHM: extract addon archive"
+cd /var
+cat new_firmware.tar.gz | gunzip | tar x
+rm new_firmware.tar.gz
+if [ ! -x /var/update_script ]; then
+  log "YAHM: Error unzipping addon image archive. Nothing to do"
+  echo "YAHM: Error unzipping addon image archive. Nothing to do"
+  exit
+fi
+
+log "YAHM: prepare mount points to simulate ccu"
+echo "YAHM: prepare mount points to simulate ccu"
+cat /var/update_script | sed -r 's/mount -t ubifs ubi0:root ([\/\w\-]*)/mount -o bind \/ \1/' > /var/update_script_prepare
+cat /var/update_script_prepare | sed -r 's/mount -t ubifs ubi1:user ([\/\w\-]*)/mount -o bind \/usr\/local \1/' > /var/update_script
+
+log "YAHM: start update_script as YAHM"
+echo "YAHM: start update_script as YAHM"
+chmod +x /var/update_script
+cd /var/
+/var/update_script CCU2
+
+EOF
